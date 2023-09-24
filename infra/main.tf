ecf8358e90398b8eafa53ff9d9c71d1b7c7af045
G751JT-CH71\999 <nate_reed@ymail.com>
2023-09-23 19:42:31 -0700

    Update -js

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
-resource "aws_lambda_function" "myfunc" {
-  filename         = data.archive_file.zip_the_python_code.output_path
-  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+resource "aws_lambda_function" "myfunc" { 
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
   function_name    = "myfunc"                  # The name of your Lambda function
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "func.handler"           # The handler function in your code
@@ -53,7 +53,7 @@ resource "aws_iam_policy" "iam_policy_for_resume_project" {
 			      "dynamodb:GetItem",
             "dynamodb:PutItem"
           ],
-          "Resource" : "arn:aws:dynamodb:*:*:table/reedcloudresumechallenge-test"
+          "Resource" : "arn:aws:dynamodb:us-west-1:547042109748:table/reedcloudresumechallenge-test"
         },
       ]
   })
@@ -65,10 +65,10 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   
 }
 
-data "archive_file" "zip_the_python_code" {
+data "archive_file" "zip" {
   type        = "zip"
-  source_file = "${path.module}/lambda/func.py"
-  output_path = "${path.module}/lambda/func.zip"
+  source_dir = "${path.module}/lambda/"
+  output_path = "${path.module}/packedlambda.zip"
 }
 
 resource "aws_lambda_function_url" "url1" {
--- infra/provider.tf
+++ infra/provider.tf
@@ -1,18 +1,14 @@
-# Define variables for AWS access key and secret key
-variable "aws_access_key" {
-  description = "AWS Access Key"
+terraform {
+  required_providers {
+    aws = {
+      version =">=4.9.0"
+      source = "hashicorp/aws"
+    }
+  }
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-}
-
-# AWS provider configuration
 provider "aws" {
   profile    = "default"  # Use the AWS CLI profile named "nate" for authentication
   region     = "us-west-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  
 }
-
-# Your other Terraform resources and configurations go here
