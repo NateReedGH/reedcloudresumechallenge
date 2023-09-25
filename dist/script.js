f63bd8794d2c24f29395b37c6c2edf33deec1134
NateReedGH <132175695+NateReedGH@users.noreply.github.com>
2023-09-25 08:23:17 -0700

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,9 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    const urls = [
-        //"https://bnycltzcvjucapaouddc6o3wg40kmvpu.lambda-url.us-west-1.on.aws/",
+    let response = await fetch(
         "https://e3bun64jkxybyyfi3lcq7ivr4q0nhixc.lambda-url.us-west-1.on.aws/"
-    ];
-
-    for (const url of urls) {
-        let response = await fetch(url);
-        let data = await response.json();
-        counter.innerHTML += `👀 Views: ${data}<br>`;
-    }
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
 }
 updateCounter();
